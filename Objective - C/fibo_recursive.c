d96dbc62b84c38b6330b2b104ff3e3076e5235d9
Hiten-js <hitensolanki010@gmail.com>
2021-03-08 14:19:26 +0530

    added Fibonacci program

---
+++ Objective - C/fibo_recursive.c
@@ -0,0 +1,22 @@
+/* 
+============================================================================
+                                O(∩_∩)O
+                           Fibonacci series
+===========================================================================
+*/
+
+// Write a program to ptint nth term of fibonnaci series upto the number
+// given by user.(Using recursion)
+// Fibonaccci series: 1, 1, 2, 3, 5, 8, _ _ _
+
+int fibonacci(int n)
+{
+     if (n <= 0)
+          printf("The number cannot be negative.");
+
+     else if (n <= 2)
+          return 1;
+
+     else
+          return fibonacci(n - 1) + fibonacci(n);
+}
